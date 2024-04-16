78f0f7421370937a63f021eba8d5d8e53e23c60b
ArvinCS <arvinciu86@yahoo.com>
2024-04-16 09:24:46 +0700

    springboot initialization

---
+++ src/test/java/id/ac/ui/cs/advprog/heymartorder/HeymartOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.heymartorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeymartOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
