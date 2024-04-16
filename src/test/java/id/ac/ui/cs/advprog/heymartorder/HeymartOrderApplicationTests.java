f281f8e47f6d6cdca128a1c90c501c341a6e1724
ArvinCS <arvinciu86@yahoo.com>
2024-04-16 09:42:07 +0700

    feat: postgres integration with ci

--- src/test/java/id/ac/ui/cs/advprog/heymartorder/HeymartOrderApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartorder/HeymartOrderApplicationTests.java
@@ -3,7 +3,7 @@ package id.ac.ui.cs.advprog.heymartorder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HeymartOrderApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HeymartOrderApplicationTests {
 
     @Test
