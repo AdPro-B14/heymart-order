f55d51683fb072db22a00a326aad31319afd73ba
meteoreinger <reikathr@gmail.com>
2024-04-24 20:37:07 +0700

    [GREEN] Fixed tests for Balance

--- src/test/java/id/ac/ui/cs/advprog/heymartorder/model/BalanceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/heymartorder/model/BalanceTest.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.heymartorder.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,17 +15,17 @@ public class BalanceTest {
 
     @BeforeEach
     void setUp() {
-        CustomerBalance customerBalance = new CustomerBalance(new Customer("Gojo"));
-        SupermarketBalance supermarketBalance = new SupermarketBalance(new Supermarket("AdproMart"));
+        this.customerBalance = new CustomerBalance(new Customer("Gojo"));
+        this.supermarketBalance = new SupermarketBalance(new Supermarket("AdproMart"));
 
-        customerBalance.setAmount(1005050);
-        supermarketBalance.setAmount(10002003.5);
+        customerBalance.setAmount(BigDecimal.valueOf(1005050));
+        supermarketBalance.setAmount(BigDecimal.valueOf(10002003.5));
     }
 
     @Test
     void testGetAmount() {
-        assertEquals(1005050, customerBalance.getAmount());
-        assertEquals(10002003.5, supermarketBalance.getAmount());
+        assertEquals(BigDecimal.valueOf(1005050), customerBalance.getAmount());
+        assertEquals(BigDecimal.valueOf(10002003.5), supermarketBalance.getAmount());
     }
 
 
