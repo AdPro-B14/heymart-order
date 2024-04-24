c788b2174700f6f9c84bebaf1f9c22a6ba365956
farrelayman09 <farrel.ayman09@gmail.com>
2024-04-25 04:02:51 +0700

    [RED] add more transactionCouponRepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/repository/TransactionCouponRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/repository/TransactionCouponRepository.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.heymartorder.repository;
 import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Repository
@@ -13,6 +14,10 @@ public class TransactionCouponRepository {
     }
 
     public TransactionCoupon findById(String id) {
+        return null;
+    }
+
+    public List<TransactionCoupon> findAll() {
         return null;
     }
 
