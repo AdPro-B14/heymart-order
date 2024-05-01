777bfb40a24744020e8357f8bc08521223af5ded
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-01 15:49:13 +0700

    [GREEN] Implemented TransactionCouponService class.

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/service/TransactionCouponServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/service/TransactionCouponServiceImpl.java
@@ -1,18 +1,39 @@
 package id.ac.ui.cs.advprog.heymartorder.service;
 
+import id.ac.ui.cs.advprog.heymartorder.factory.TransactionCouponFactory;
 import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
 import id.ac.ui.cs.advprog.heymartorder.repository.TransactionCouponRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class TransactionCouponServiceImpl implements TransactionCouponService {
     @Autowired
     TransactionCouponRepository transactionCouponRepository;
     @Override
-    public TransactionCoupon createTransactionCoupon(TransactionCoupon tcCoupon) {return null;}
+    public TransactionCoupon createTransactionCoupon(TransactionCoupon tcCoupon) {
+        if (transactionCouponRepository.findById(tcCoupon.getCouponId()) == null) {
+            transactionCouponRepository.save(tcCoupon);
+            return tcCoupon;
+        }
+        return null;
+    }
     @Override
-    public TransactionCoupon updateIsUsed(String couponId, boolean isUsed) {return null;}
+    public TransactionCoupon updateIsUsed(String couponId, boolean isUsed) {
+        TransactionCoupon tcCoupon = transactionCouponRepository.findById(couponId);
+        if (tcCoupon != null) {
+            tcCoupon.setUsed(isUsed); // Update the existing coupon
+            transactionCouponRepository.save(tcCoupon); // Save the updated coupon
+            return tcCoupon;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
     @Override
-    public TransactionCoupon findById(String couponId) {return null;}
+    public TransactionCoupon findById(String couponId) {
+        return transactionCouponRepository.findById(couponId);
+    }
 }
