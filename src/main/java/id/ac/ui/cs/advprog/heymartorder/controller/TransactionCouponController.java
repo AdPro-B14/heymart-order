1a27131bbe2fc4009c7bd8dda6a303baaf4df49e
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-01 20:54:50 +0700

    add PostMapping for addTransactionCoupon

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
@@ -1,13 +1,17 @@
 package id.ac.ui.cs.advprog.heymartorder.controller;
 
+import id.ac.ui.cs.advprog.heymartorder.factory.TransactionCouponFactory;
 import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
 import id.ac.ui.cs.advprog.heymartorder.repository.TransactionCouponRepository;
 import id.ac.ui.cs.advprog.heymartorder.service.JwtService;
 import id.ac.ui.cs.advprog.heymartorder.service.TransactionCouponService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import id.ac.ui.cs.advprog.heymartorder.dto.AddTransactionCouponRequest;
+
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -23,4 +27,15 @@ public class TransactionCouponController {
         List<TransactionCoupon> allTransactionCoupon = transactionCouponService.findAll();
         return ResponseEntity.ok(allTransactionCoupon);
     }
+    @PostMapping("/create-transaction-coupon")
+    public ResponseEntity<TransactionCoupon> addTransactionCoupon(@RequestBody AddTransactionCouponRequest tcCoupon) {
+        TransactionCouponFactory transactionCouponFactory = new TransactionCouponFactory();
+
+        TransactionCoupon transactionCoupon1 = transactionCouponFactory
+                .orderCoupon(tcCoupon.couponId, tcCoupon.couponName,
+                        tcCoupon.couponNominal, tcCoupon.isUsed, tcCoupon.minimumBuy);
+
+        TransactionCoupon savedTcCoupon = transactionCouponService.createTransactionCoupon(transactionCoupon1);
+        return new ResponseEntity<>(savedTcCoupon, HttpStatus.OK);
+    }   
 }
