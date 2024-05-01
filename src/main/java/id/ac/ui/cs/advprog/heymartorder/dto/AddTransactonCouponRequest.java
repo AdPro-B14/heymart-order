11ede765c50780b74ba7c23d57e384b0f4c66853
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-01 20:11:36 +0700

    [DTO] add AddTransactonCouponRequest dto

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/AddTransactonCouponRequest.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.heymartorder.dto;
+
+public class AddTransactonCouponRequest {
+    public String couponId;
+    String couponName;
+    long couponNominal;
+    long minimumBuy;
+    boolean isUsed;
+    String productId;
+}
