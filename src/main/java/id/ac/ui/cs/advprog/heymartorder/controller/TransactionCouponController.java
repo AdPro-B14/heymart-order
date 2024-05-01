793808d83443066fc7deb2a71e089392c03e8eb8
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-01 17:00:02 +0700

    [CONTROLLER] add TransactionCoupon controller

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
@@ -1,4 +1,26 @@
 package id.ac.ui.cs.advprog.heymartorder.controller;
 
-public class CouponController {
+import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
+import id.ac.ui.cs.advprog.heymartorder.repository.TransactionCouponRepository;
+import id.ac.ui.cs.advprog.heymartorder.service.JwtService;
+import id.ac.ui.cs.advprog.heymartorder.service.TransactionCouponService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@RestController
+public class TransactionCouponController {
+
+    @Autowired
+    private TransactionCouponService transactionCouponService;
+
+    @GetMapping("/all-transaction-coupon")
+    public ResponseEntity<List<TransactionCoupon>> allTransactionCoupon() {
+        List<TransactionCoupon> allTransactionCoupon = transactionCouponService.findAll();
+        return ResponseEntity.ok(allTransactionCoupon);
+    }
 }
