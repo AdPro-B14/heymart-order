afd24d79236d55ac3f0f82d500be9fb5168e095d
raissaputi <raissa.puti@gmail.com>
2024-05-15 19:57:08 +0700

    feat: ci/cd for environment based deployment

---
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/GetProductResponse.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.heymartorder.dto;
+
+public class GetProductResponse {
+    public String UUID;
+    public String nama;
+    public Integer stock;
+    public Long price;
+
+}
