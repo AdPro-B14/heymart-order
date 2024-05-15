34915011563b50d5e93b39b4e9089cc4fcf810dc
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-15 13:30:35 +0700

    [REFACTOR] refactor models of Coupons to fit JpaRepository findByCouponId method

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/model/Coupon.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/model/Coupon.java
@@ -1,12 +1,21 @@
 package id.ac.ui.cs.advprog.heymartorder.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     String couponId;
+
     String couponName;
     long couponNominal;
     long minimumBuy;
@@ -19,6 +28,9 @@ public abstract class Coupon {
         this.couponNominal = couponNominal;
     }
 
-    public abstract void prepare();
+    Coupon() {
+        // Default constructor
+    }
 
+    public abstract void prepare();
 }
--- src/main/java/id/ac/ui/cs/advprog/heymartorder/model/TransactionCoupon.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/model/TransactionCoupon.java
@@ -1,19 +1,26 @@
 package id.ac.ui.cs.advprog.heymartorder.model;
 
-import id.ac.ui.cs.advprog.heymartorder.factory.TransactionCouponFactory;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class TransactionCoupon extends Coupon{
+@Entity
+public class TransactionCoupon extends Coupon {
+
     public TransactionCoupon(String couponId, String couponName, Long couponNominal, boolean isUsed, Long minimumBuy) {
         super(couponId, couponName, couponNominal);
         this.isUsed = isUsed;
         this.minimumBuy = minimumBuy;
     }
 
-    public void prepare() {
+    protected TransactionCoupon() {
+        super();
+    }
 
+    @Override
+    public void prepare() {
+        // Implementation of prepare method
     }
 }
