92441194fc0897927199251aca97ddbbaf0c57d8
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-18 10:28:18 +0700

    [RED] ProductCouponService & ProductCouponServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/service/ProductCouponService.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/service/ProductCouponService.java
@@ -1,2 +1,13 @@
-package id.ac.ui.cs.advprog.heymartorder.service;public interface ProductCouponService {
+package id.ac.ui.cs.advprog.heymartorder.service;
+
+import id.ac.ui.cs.advprog.heymartorder.model.ProductCoupon;
+import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
+
+import java.util.List;
+
+public interface ProductCouponService {
+    public ProductCoupon createProductCoupon(ProductCoupon tcCoupon);
+    public ProductCoupon findById(String couponId);
+    public List<ProductCoupon> findAll();
+    public void delete(String id);
 }
--- src/main/java/id/ac/ui/cs/advprog/heymartorder/service/ProductCouponServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/service/ProductCouponServiceImpl.java
@@ -1,2 +1,44 @@
-package id.ac.ui.cs.advprog.heymartorder.service;public class ProductCouponServiceImpl {
+package id.ac.ui.cs.advprog.heymartorder.service;
+
+import id.ac.ui.cs.advprog.heymartorder.model.ProductCoupon;
+import id.ac.ui.cs.advprog.heymartorder.repository.ProductCouponRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@Service
+public class ProductCouponServiceImpl implements ProductCouponService {
+    @Autowired
+    ProductCouponRepository productCouponRepository;
+    @Override
+    public ProductCoupon createProductCoupon(ProductCoupon pCoupon) {
+        if (pCoupon == null) {
+            throw new IllegalArgumentException();
+        } else {
+            productCouponRepository.save(pCoupon);
+            return pCoupon;
+        }
+    }
+
+    @Override
+    public ProductCoupon findById(String couponId) {
+        return productCouponRepository.findByCouponId(couponId);
+    }
+
+    @Override
+    public List<ProductCoupon> findAll() {
+        return productCouponRepository.findAll();
+    }
+
+    @Override
+    public void delete(String id) {
+        if (id == null) {
+            throw new IllegalArgumentException();
+        } else {
+            ProductCoupon pCoupon = findById(id);
+            productCouponRepository.delete(pCoupon);
+        }
+    }
 }
