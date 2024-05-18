351da1f2b99fea7bf0054302c9fe6e72ef8d4131
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-18 17:36:18 +0700

    [RED] add ProductCouponController and modify TransactionCouponController

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/controller/TransactionCouponController.java
@@ -6,6 +6,7 @@ import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
 import id.ac.ui.cs.advprog.heymartorder.repository.TransactionCouponRepository;
 import id.ac.ui.cs.advprog.heymartorder.service.JwtService;
 import id.ac.ui.cs.advprog.heymartorder.service.TransactionCouponService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -19,6 +20,8 @@ import org.springframework.web.reactive.function.client.WebClient;
 import java.util.List;
 
 @RestController
+@RequestMapping("/transaction-coupon")
+@RequiredArgsConstructor
 public class TransactionCouponController {
 
     @Value("${spring.route.gateway_url}")
@@ -26,18 +29,25 @@ public class TransactionCouponController {
 
     private final JwtService jwtService;
 
-    private final WebClient webClient;
+    private final TransactionCouponService transactionCouponService;
 
-    private TransactionCouponService transactionCouponService;
 
-    public TransactionCouponController(JwtService jwtService, WebClient webClient) {
-        this.jwtService = jwtService;
-        this.webClient = webClient;
+
+    @GetMapping("/transaction-coupon/{id}")
+    public ResponseEntity<TransactionCoupon> getTransactionCoupon(@PathVariable("id") String couponId) {
+        TransactionCoupon tcCoupon = transactionCouponService.findById(couponId);
+        return ResponseEntity.ok(tcCoupon);
+    }
+
+    @GetMapping("/supermarket-transaction-coupon/{id}")
+    public ResponseEntity<List<TransactionCoupon>> getSupermarketTransactionCoupons(@PathVariable("id") Long supermarketId) {
+        List<TransactionCoupon> allTransactionCoupon = transactionCouponService.findBySupermarketId(supermarketId);
+        return ResponseEntity.ok(allTransactionCoupon);
     }
 
     // supermarketId /{id}
     @GetMapping("/all-transaction-coupon")
-    public ResponseEntity<List<TransactionCoupon>> allTransactionCoupon() {
+    public ResponseEntity<List<TransactionCoupon>> getAllTransactionCoupons() {
         List<TransactionCoupon> allTransactionCoupon = transactionCouponService.findAll();
         return ResponseEntity.ok(allTransactionCoupon);
     }
@@ -55,13 +65,13 @@ public class TransactionCouponController {
 
         TransactionCouponFactory transactionCouponFactory = new TransactionCouponFactory();
         TransactionCoupon tcCoupon = transactionCouponFactory
-                .orderCoupon(request.couponId, request.supermarketId, request.couponName, request.couponNominal, request.isUsed, request.minimumBuy);
+                .orderCoupon(request.supermarketId, request.couponName, request.couponNominal, request.minimumBuy);
 
         return ResponseEntity.ok(transactionCouponService.createTransactionCoupon(tcCoupon));
     }
 
     @DeleteMapping("/delete-transaction-coupon/{id}")
-    public ResponseEntity<SuccessResponse> deleteSupermarket(@RequestHeader(value = "Authorization") String id,
+    public ResponseEntity<SuccessResponse> deleteTransactionCoupon(@RequestHeader(value = "Authorization") String id,
                                                              @PathVariable("id") String couponId) throws IllegalAccessException {
         String token = id.replace("Bearer ", "");
         if (!jwtService.extractRole(token).equalsIgnoreCase("manager")
@@ -70,7 +80,30 @@ public class TransactionCouponController {
         }
 
         transactionCouponService.delete(couponId);
-        return ResponseEntity.ok(SuccessResponse.builder().success(true).build());    }
+        return ResponseEntity.ok(SuccessResponse.builder().success(true).build());
+    }
 
-    //getProduct
+    @GetMapping("/transaction-coupon-consumers/{id}")
+    public ResponseEntity<List<Long>> getTransactionCouponConsumers(@RequestHeader(value = "Authorization") String id,
+                                                                    @PathVariable("id") String couponId) throws IllegalAccessException {
+        String token = id.replace("Bearer ", "");
+        if (!jwtService.extractRole(token).equalsIgnoreCase("manager")
+                && !jwtService.extractRole(token).equalsIgnoreCase("admin")) {
+            throw new IllegalAccessException("You have no access.");
+        }
+
+        return ResponseEntity.ok(transactionCouponService.getConsumers(couponId));
+    }
+
+    // tanya dulu lagi, pake/ngga?
+//    @GetMapping("/transaction-coupon-validation") //gaperlu dto, response aja
+//    public ResponseEntity<SuccessResponse> validateCoupon(@RequestParam("couponId") String couponId, @RequestParam("consumerId") Long consumerId) {
+//        return ResponseEntity.ok(SuccessResponse.builder().success(true).build());
+//    }
+
+    // validasi make service validate consumer
+    @GetMapping("/transaction-coupon-validation")
+    public ResponseEntity<Boolean> validateCoupon(@RequestParam("couponId") String couponId, @RequestParam("consumerId") Long consumerId) {
+        return ResponseEntity.ok(transactionCouponService.validateConsumer(couponId, consumerId));
+    }
 }
