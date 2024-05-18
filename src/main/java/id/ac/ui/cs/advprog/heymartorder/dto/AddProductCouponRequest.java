4ce89a3390f04db30dcd2556129664a3a6a0987f
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-18 17:35:31 +0700

    [DTO] change dto into correct forms

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/AddProductCouponRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/AddProductCouponRequest.java
@@ -1,2 +1,11 @@
-package id.ac.ui.cs.advprog.heymartorder.dto;public class AddProductCouponRequest {
+package id.ac.ui.cs.advprog.heymartorder.dto;
+
+import lombok.Builder;
+
+@Builder
+public class AddProductCouponRequest {
+    public Long supermarketId;
+    public String couponName;
+    public long couponNominal;
+    public String productId;
 }
--- src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/AddTransactionCouponRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/dto/AddTransactionCouponRequest.java
@@ -1,10 +1,15 @@
 package id.ac.ui.cs.advprog.heymartorder.dto;
 
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
 public class AddTransactionCouponRequest {
-    public String couponId;
     public Long supermarketId;
     public String couponName;
     public long couponNominal;
     public long minimumBuy;
-    public boolean isUsed;
+//    public boolean isUsed;
+    public List<Long> consumers;
 }
