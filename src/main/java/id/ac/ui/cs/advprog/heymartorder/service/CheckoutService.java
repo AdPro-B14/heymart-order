10f45f9651adcf0d139c6570b5bdb08cc2a87ed5
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-25 11:35:51 +0700

    add checkoutWithCoupon logic

--- src/main/java/id/ac/ui/cs/advprog/heymartorder/service/CheckoutService.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/service/CheckoutService.java
@@ -1,5 +1,9 @@
 package id.ac.ui.cs.advprog.heymartorder.service;
 
+import id.ac.ui.cs.advprog.heymartorder.model.ProductCoupon;
+import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
+
 public interface CheckoutService {
-    boolean checkout(Long userId);
+    boolean checkout(Long userId, String token);
+    Long checkoutWithCoupon(Long userId, String token, ProductCoupon productCoupon, TransactionCoupon transactionCoupon);
 }
--- src/main/java/id/ac/ui/cs/advprog/heymartorder/service/CheckoutServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/heymartorder/service/CheckoutServiceImpl.java
@@ -1,7 +1,12 @@
 package id.ac.ui.cs.advprog.heymartorder.service;
 
+import id.ac.ui.cs.advprog.heymartorder.model.KeranjangItem;
+import id.ac.ui.cs.advprog.heymartorder.model.ProductCoupon;
+import id.ac.ui.cs.advprog.heymartorder.model.TransactionCoupon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
@@ -9,9 +14,9 @@ public class CheckoutServiceImpl implements CheckoutService {
     private KeranjangBelanjaServiceImpl keranjangBelanjaService;
 
     @Override
-    public boolean checkout(Long userId) {
+    public boolean checkout(Long userId, String token) {
         try {
-            Long total = keranjangBelanjaService.countTotal(userId);
+            Long total = keranjangBelanjaService.countTotal(userId, token);
 
             /*
             TODO
@@ -21,6 +26,55 @@ public class CheckoutServiceImpl implements CheckoutService {
             return true;
         } catch (Exception e) {
             return false;
+        }
+    }
+
+    @Override
+    public Long checkoutWithCoupon(Long userId, String token, ProductCoupon productCoupon, TransactionCoupon transactionCoupon) {
+        try {
+            Long total = keranjangBelanjaService.countTotal(userId, token);
+
+            if (productCoupon != null) {
+                total = applyProductCoupon(userId, token, productCoupon);
+            }
+            if (transactionCoupon != null) {
+                total = applyTransactionCoupon(userId, token, transactionCoupon);
+            }
+
+            keranjangBelanjaService.clearKeranjang(userId);
+            return total;
+        } catch (Exception e) {
+            return 0L;
+        }
+    }
+
+    public Long applyProductCoupon(Long userId, String token, ProductCoupon productCoupon) {
+        try {
+            List<KeranjangItem> items = keranjangBelanjaService.findKeranjangById(userId).getListKeranjangItem();
+            Long currentTotal = keranjangBelanjaService.countTotal(userId, token);
+            for (KeranjangItem i : items) {
+                if (i.getProductId().equals(productCoupon.getProductId())) {
+                    long couponSum = productCoupon.getCouponNominal() * i.getAmount();
+                    currentTotal -= couponSum;
+                }
+            }
+            return currentTotal;
+        } catch (Exception e) {
+            return 0L;
+        }
+    }
+
+    public Long applyTransactionCoupon(Long userId, String token, TransactionCoupon transactionCoupon) {
+        try {
+            Long currentTotal = keranjangBelanjaService.countTotal(userId, token);
+            if (currentTotal >= transactionCoupon.getMinimumBuy()) {
+                return currentTotal - transactionCoupon.getCouponNominal();
+            }
+            else {
+                throw new IllegalArgumentException("Cart total is not enough to use coupon");
+            }
+        } catch (Exception e) {
+            return 0L;
         }
     }
 
